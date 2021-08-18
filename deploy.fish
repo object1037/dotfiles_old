ffb30e704089e1a68dcebf18e499cdb29236b35a
object1037 <contact@object1037.dev>
2021-08-18 14:52:13 +0900

    delete install.fish

--- deploy.fish
+++ deploy.fish
@@ -2,6 +2,16 @@
 
 set --local dir (pwd)
 
+function has -a CMD_NAME
+  type "$CMD_NAME" > /dev/null 2>&1
+end
+
+function info -a MSG
+  echo -n "["
+  set_color blue; echo -n "INFO"
+  set_color normal; echo "] $MSG"
+end
+
 # fish
 for file in (ls -A fish | grep -v ".DS_Store")
     ln -nsf "$dir/fish/$file" "$HOME/.config/fish/$file"
